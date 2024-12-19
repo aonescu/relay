c64cef9953fc0a75c2f955bde7fbe1b8e83416ca
OnescuAlex-Vlad <onescu8779@gmail.com>
2024-12-20 00:51:52 +0200

    added manager, task and node structs

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"github.com/aonescu/relay/node"
+	"github.com/aonescu/relay/task"
+	"fmt"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+
+	"github.com/aonescu/relay/manager"
+	"github.com/aonescu/relay/worker"
+)
+
 func main() {
-	Start()
+	t := task.Task{
+		ID:     uuid.New(),
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
+
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+
+	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
+
+	w := worker.Worker{
+		Name:  "worker-1",
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
+	}
+	fmt.Printf("worker: %v\n", w)
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDb:  make(map[string][]*task.Task),
+		EventDb: make(map[string][]*task.TaskEvent),
+		Workers: []string{w.Name},
+	}
+
+	fmt.Printf("manager: %v\n", m)
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("node: %v\n", n)
 }
